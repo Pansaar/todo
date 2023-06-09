6af245fd04204eaf550a8c5bc4beba57c0b2b4e1
magicdev32 <81415343+Pansaar@users.noreply.github.com>
2023-06-10 01:27:01 +0700

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,57 @@
+const inputList = document.getElementById("input");
+const listContainer = document.getElementById("list-container")
+
+
+function addTask() {
+    if(inputList.value === '') {
+        alert("Input Task!")
+
+
+    }
+
+    else {
+
+        let li = document.createElement("li");
+        li.innerHTML = inputList.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = " \u00d7";
+        li.appendChild(span);
+
+    }
+
+    inputList.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked")
+        saveData()
+
+    }
+
+    else if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked")
+
+
+    }
+
+    else if(e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData()
+    }
+
+
+},false)
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+
+}
+
+showTask() 
